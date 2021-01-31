34ddb133f5de20fea94e5bbd988782566204286a
freeman-lab <the.freeman.lab@gmail.com>
2021-01-31 01:09:39 -0800

    rename

--- src/meta.js
+++ src/meta.js
@@ -62,7 +62,7 @@ const Meta = ({ title, description, card }) => {
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='https://images.carbonplan.org/favicon-180x180.png'
+        href='https://images.carbonplan.org/apple-touch-icon.png'
       />
       <meta name='msapplication-TileColor' content='#1b1e23' />
       <meta
