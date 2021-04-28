018ff41304647a01db8e4f660d493046792e59b6
freeman-lab <the.freeman.lab@gmail.com>
2021-04-27 20:44:25 -0700

    fix bug in date formatting

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,7 +1,7 @@
 const formatDate = (date) => {
   let d = new Date(date.replace(/-/g, '/'))
   let month = d.toLocaleString('default', { month: 'short' })
-  let day = String(d.getDay()).padStart(2, '0')
+  let day = String(d.getDate()).padStart(2, '0')
   let year = d.getFullYear()
   return month + ' ' + day + ' ' + year
 }
