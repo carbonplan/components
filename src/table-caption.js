11279dbdf73825d1f891340d6bb73da0aa772316
Kata Martin <katasm@gmail.com>
2022-03-21 16:25:32 -0700

    Use figcaption as default for TableCaption
    Because Table does not easily support the nesting of Table caption within it,
    expect to use TableCaption independently

--- src/table-caption.js
+++ src/table-caption.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Caption from './caption'
 
-const TableCaption = ({ as = 'caption', number, children }) => {
+const TableCaption = ({ as = 'figcaption', number, children }) => {
   return (
     <Caption as={as} number={number} label='table'>
       {children}
