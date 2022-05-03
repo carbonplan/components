f8469a6018fab4860a997e98485a313e6ac19451
Kata Martin <katasm@gmail.com>
2022-05-03 09:58:18 -0700

    Only render filtered children when special character present

--- src/blockquote.js
+++ src/blockquote.js
@@ -5,6 +5,7 @@ const specialChars = ['“', '"', "'", '‘']
 
 const Blockquote = ({ children }) => {
   let firstChar = ''
+  let filteredChildren = children
 
   if (
     Array.isArray(children) &&
@@ -12,19 +13,19 @@ const Blockquote = ({ children }) => {
     typeof children[0].props.children === 'string'
   ) {
     firstChar = children[0].props.children.slice(0, 1)
-    children = Children.map(children, (d, i) => {
+    filteredChildren = Children.map(children, (d, i) => {
       if (i == 0) {
         return cloneElement(d, { children: d.props.children.slice(1) })
       } else return d
     })
   } else if (children.props && typeof children.props.children === 'string') {
     firstChar = children.props.children.slice(0, 1)
-    children = cloneElement(children, {
+    filteredChildren = cloneElement(children, {
       children: children.props.children.slice(1),
     })
   } else if (typeof children === 'string') {
     firstChar = children.slice(0, 1)
-    children = children.slice(1)
+    filteredChildren = children.slice(1)
   }
 
   return (
@@ -34,7 +35,7 @@ const Blockquote = ({ children }) => {
           {firstChar}
         </Box>
       )}
-      {children}
+      {specialChars.includes(firstChar) ? filteredChildren : children}
     </Box>
   )
 }
